931bad344c861a9fba7c57f9a65c2f982415e68c
mantuonweb <mantuonweb@hotmail.com>
2017-12-03 20:45:35 +0530

    added files eject

--- ngwebworker/src/app/app.module.ts
+++ ngwebworker/src/app/app.module.ts
@@ -1,4 +1,5 @@
-import { BrowserModule } from '@angular/platform-browser';
+//import { BrowserModule } from '@angular/platform-browser'; //-Remove
+import { WorkerAppModule } from '@angular/platform-webworker'; //++Add
 import { NgModule } from '@angular/core';
 
 
@@ -6,12 +7,13 @@ import { AppComponent } from './app.component';
 
 //https://stackoverflow.com/questions/43276044/angular-cli-generated-app-with-web-
 //https://blog.angularindepth.com/angular-with-web-workers-step-by-step-dc11d5872135
+//http://www.syntaxsuccess.com/viewarticle/web-workers-in-angular-2.0
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    WorkerAppModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- ngwebworker/src/main.ts
+++ ngwebworker/src/main.ts
@@ -1,5 +1,6 @@
 import { enableProdMode } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+//import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';//-delete
+import { bootstrapWorkerUi } from '@angular/platform-webworker';//+add
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
@@ -7,6 +8,8 @@ import { environment } from './environments/environment';
 if (environment.production) {
   enableProdMode();
 }
+if (environment.production) {
+  enableProdMode();
+}
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.log(err));
+bootstrapWorkerUi('../webworker.bundle.js');
